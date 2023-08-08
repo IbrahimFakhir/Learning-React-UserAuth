2f1fa7b87fe66518455039a509f8d0e940ed4d11
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-08 16:55:35 +0200

    basic login component

--- tutorial-02/src/Login.jsx
+++ tutorial-02/src/Login.jsx
@@ -1,6 +1,11 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "./context/AuthProvider";
+import axios from './api/axios';
+
+const LOGIN_URL = '/auth';
 
 function Login() {
+	const { setAuth } = useContext(AuthContext);
 	const userRef = useRef();
 	const errRef = useRef();
 
@@ -17,35 +22,68 @@ function Login() {
 		setErrMsg('');
 	}, [user, pwd]);
 
-	return (
-		<section>
-			<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
-				{errMsg}
-			</p>
-			<h1>Sign In</h1>
-			<form>
-				<label htmlFor="username">Username:</label>
-				<input 
-					type="text"
-					id="username"
-					ref={userRef}
-					autoComplete="off"
-					onChange={(e) => setUser(e.target.value)}
-					value={user}
-					required
-				/>
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-				<label htmlFor="password">Password:</label>
-				<input 
-					type="password"
-					id="password"
-					onChange={(e) => setPwd(e.target.value)}
-					value={pwd}
-					required
-				/>
-			</form>
-		</section>
+		console.log(user, pwd);
+		setUser('');
+		setPwd('');
+		setSuccess(true);
+	}
+
+	return (
+		<>
+			{success ? (
+				<section>
+					<h1>You are logged in!</h1>
+					<br />
+					<p>
+						<a href="#">Go to Home</a>
+					</p>
+				</section>
+			) : (
+				<section>
+					<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+						{errMsg}
+					</p>
+					<h1>Sign In</h1>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="username">Username:</label>
+						<input 
+							type="text"
+							id="username"
+							ref={userRef}
+							autoComplete="off"
+							onChange={(e) => setUser(e.target.value)}
+							value={user}
+							required
+						/>
+
+						<label htmlFor="password">Password:</label>
+						<input 
+							type="password"
+							id="password"
+							onChange={(e) => setPwd(e.target.value)}
+							value={pwd}
+							required
+						/>
+
+						<button>
+							Sign In
+						</button>
+					</form>
+					<p>
+						Need an Account?<br />
+						<span className="line">
+							{/* React Router Link here */}
+							<a href="#">Sign Up</a>
+						</span>
+					</p>
+				</section>
+				)
+			}
+		</>
 	)
 }
 
-export default Login
+export default Login;
